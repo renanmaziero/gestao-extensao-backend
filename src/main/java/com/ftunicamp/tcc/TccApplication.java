7cda9fdc24c356d6c0ebd654f1cbec811d705cb4
renanmaziero <renanfmaziero@gmail.com>
2022-07-25 21:16:37 -0300

    v1.01.5 (stack heroku-22)

--- src/main/java/com/ftunicamp/tcc/TccApplication.java
+++ src/main/java/com/ftunicamp/tcc/TccApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class TccApplication {
-
+//Sistema GAS v1.01.5 (stack heroku-22)
     public static void main(String[] args) {
 		SpringApplication.run(TccApplication.class, args);
 	}
